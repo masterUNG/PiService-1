22b1b571a598a135b06ae6e00faa737c2ceb3e52
piyawat <kct.thai@gmail.com>
2017-10-27 16:00:58 +0700

    finish 27oct

--- app/src/main/java/kct/piyawat/piservice/MainActivity.java
+++ app/src/main/java/kct/piyawat/piservice/MainActivity.java
@@ -3,11 +3,22 @@ package kct.piyawat.piservice;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import kct.piyawat.piservice.Fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+   //     Add Fragment to Activity
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentFragmentMain,new MainFragment()).commit();
+        }
+
+
+
+    }   // Main Method
+} // Main Class
