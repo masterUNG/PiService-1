1f3d656e52d6638471843235d14e8f1a809b3b8d
piyawat <kct.thai@gmail.com>
2017-10-31 15:58:16 +0700

    Finish 31 oct

--- app/src/main/java/kct/piyawat/piservice/Fragment/MainFragment.java
+++ app/src/main/java/kct/piyawat/piservice/Fragment/MainFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import kct.piyawat.piservice.R;
 
@@ -13,12 +14,46 @@ import kct.piyawat.piservice.R;
  * Created by asus on 27/10/2560.
  */
 
-public class MainFragment extends Fragment{
+public class MainFragment extends Fragment {
+
+//    Create Main Method
 
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Register Controller
+        registerController();
+
+    } // Main Method
+
+    private void registerController() {
+//        Initial View
+
+        TextView textView = getView().findViewById(R.id.txtNewRegister);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+//                Replace Fragment เปลี่ยนหน้ากา
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.contentFragmentMain,new RegisterFragment())
+                        .addToBackStack(null)
+                        .commit()
+
+
+            }
+        });
+    }
+
+    //Create View หรือ การสร้างหน้ากาก
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
         return view;
     }
